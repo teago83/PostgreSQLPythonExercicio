dbe1fcf9e50da8d90acfbb45dbdc83f6de9c4a53
teago83 <supremeleaderpastel@gmail.com>
2019-10-09 16:24:53 -0300

    Início do CRUD na versão para PostgreSQL. Não tem dado muito certo e a parte do 'update' precisa ser desenvolvida, assim, como a do delete e o menu em si.

--- operacao_banco.py
+++ operacao_banco.py
@@ -1,5 +1,5 @@
 import psycopg2
-from connection import Connection
+from PostgreSQLPythonExercicio.connection import Connection
 
 #pip install psycopg2
 
@@ -39,3 +39,51 @@ class Operacoes():
             if (connection):
                 cursor.close()
                 connection.close()
+
+"""
+class Operacoes():
+    
+    def salvar(self, nome, sobrenome, matricula):
+        connection = Connection().get_connection()
+        cursor = connection.cursor()
+        insert = """"insert into aluno (matricula, nome, sobrenome) values ('{0}', '{1}', '{2}');""".format(nome, sobrenome, matricula)"""
+        cursor.execute(insert)
+        connection.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print("Error", error)
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
+
+    def buscar(self):
+        try:
+            connection = Connection().get_connection()
+            cursor = connection.cursor()
+            select = "select * from aluno"
+            cursor.execute(select)
+            alunos = cursor.fetchball()
+
+            for row in alunos:
+                print("Matrícula = ", row[0], )
+                print("Nome = ", row[1])
+                print("Sobrenome = ", row[2], "\n")
+
+        except (Exception, psycopg2.Error) as error:
+            print("Error", error)
+        finally:
+            if connection:
+                cursor.close()
+                connection.close()
+    
+    def atualizar(self, matricula, nome, sobrenome):
+        """atualizar aluno baseando-se em seus dados"""
+        sql = """ UPDATE alunos
+                    SET nome = %s 
+                    SET sobrenome = %s 
+                    SET matricula = %d 
+                    WHERE matricula = %d"""
+        connection = None
+        updated_rows = 0
+        try:
+            params = config()
